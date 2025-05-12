12d69440deeb5e56034a1f15543641529704642f
Leonardo Lourenço Crespilho <lcrespilho@gmail.com>
2025-05-12 09:24:05 -0300

    feat: inclusão de GA4

--- popup.js
+++ popup.js
@@ -1,12 +1,13 @@
-function getStoredData(gtmIds, callback) {
-  chrome.storage.local.get(gtmIds, result => {
+function getStoredData(ids, callback) {
+  chrome.storage.local.get(ids, result => {
     const data = {}
-    gtmIds.forEach(gtmId => {
+    ids.forEach(id => {
       // Default values if none stored
-      const url = result[gtmId]?.url ?? ''
-      const description = result[gtmId]?.description ?? ''
-      const authuser = result[gtmId]?.authuser || (url && new URL(url).searchParams.get('authuser')) || '0'
-      data[gtmId] = { url, description, authuser }
+      const url = result[id]?.url ?? ''
+      const description = result[id]?.description ?? ''
+      const authuser = result[id]?.authuser || (url && new URL(url).searchParams.get('authuser')) || '0'
+
+      data[id] = { url, description, authuser }
     })
     callback(data)
   })
@@ -20,37 +21,42 @@ chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
         target: { tabId: currentTab.id },
         world: 'MAIN',
         function: () => {
-          const gtmIds = new Set()
+          const gtms = []
           if (window.google_tag_manager) {
             for (const key in window.google_tag_manager) {
               if (key.startsWith('GTM-')) {
-                gtmIds.add(key)
+                gtms.push(key)
               }
             }
           }
-          return Array.from(gtmIds)
+          const gaIds = performance
+            .getEntriesByType('resource')
+            .filter(e => e.name.includes('gtag/js?id=G-'))
+            .map(e => e.name.match(/G-\w+/)[0])
+          return [...gtms, ...gaIds]
         },
       },
       results => {
         const gtmList = document.getElementById('gtm-list')
-        const gtms = results[0].result
+        const gaList = document.getElementById('ga-list')
+        const containers = results[0].result // GTMs e GTAGs
 
-        if (gtms && gtms.length > 0) {
+        if (containers && containers.length > 0) {
           // Get stored data before creating the list
-          getStoredData(gtms, data => {
-            gtms.forEach(gtmId => {
-              const url = data[gtmId].url
-              const description = data[gtmId].description
-              const authuser = data[gtmId].authuser
+          getStoredData(containers, data => {
+            containers.forEach(containerId => {
+              const url = data[containerId].url
+              const description = data[containerId].description
+              const authuser = data[containerId].authuser
               const listItem = document.createElement('li')
-              listItem.className = 'gtm-item'
+              const containerType = containerId.startsWith('GTM-') ? 'gtm' : 'ga'
               listItem.innerHTML = `
                 <div style="margin-bottom: 8px;">
-                  <span>${gtmId}</span>
+                  <span>${containerId}</span>
                   <input
                     type="text"
                     class="description-input"
-                    data-gtmid="${gtmId}"
+                    data-containerid="${containerId}"
                     value="${description}"
                     placeholder="Description"
                     style="width: 300px; margin: 0 8px; padding: 4px;"
@@ -60,73 +66,75 @@ chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
                   <input
                     type="text"
                     class="url-input"
-                    data-gtmid="${gtmId}"
+                    data-containerid="${containerId}"
                     value="${url}"
                     style="width: 220px; margin: 0 8px; padding: 4px;"
                   />
                   <input
                     type="text"
                     class="authuser-input"
-                    data-gtmid="${gtmId}"
+                    data-containerid="${containerId}"
                     value="${authuser}"
                     placeholder="authuser"
-                    style="width: 40px; padding: 4px; text-aligh: center; margin-right: 8px;"
+                    style="width: 40px; padding: 4px; text-align: center; margin-right: 8px;"
                   >
-                  <button data-gtmid="${gtmId}">Abrir</button>
+                  <button data-containerid="${containerId}">Abrir</button>
                 </div>
               `
-              gtmList.appendChild(listItem)
+              ;(containerType === 'gtm' ? gtmList : gaList).appendChild(listItem)
             })
 
             // Add event listeners for description input changes
             document.querySelectorAll('.description-input').forEach(input =>
               input.addEventListener('change', e => {
-                const gtmId = e.target.dataset.gtmid
+                const containerId = e.target.dataset.gtmid
                 const description = e.target.value
-                const url = document.querySelector(`.url-input[data-gtmid="${gtmId}"]`).value
-                const authuser = document.querySelector(`.authuser-input[data-gtmid="${gtmId}"]`).value
-                chrome.storage.local.set({ [gtmId]: { url, description, authuser } })
+                const url = document.querySelector(`.url-input[data-containerid="${containerId}"]`).value
+                const authuser = document.querySelector(`.authuser-input[data-containerid="${containerId}"]`).value
+                chrome.storage.local.set({ [containerId]: { url, description, authuser } })
               })
             )
 
             // Add event listeners for URL input changes
             document.querySelectorAll('.url-input').forEach(input =>
               input.addEventListener('change', e => {
-                const gtmId = e.target.dataset.gtmid
-                const description = document.querySelector(`.description-input[data-gtmid="${gtmId}"]`).input
+                const containerId = e.target.dataset.gtmid
+                const description = document.querySelector(
+                  `.description-input[data-containerid="${containerId}"]`
+                ).value
                 const url = new URL(e.target.value) // quebra se url inválida
                 const authuser =
                   url.searchParams.get('authuser') ||
-                  document.querySelector('.authuser-input[data-gtmid="${gtmId}"]').value ||
+                  document.querySelector('.authuser-input[data-containerid="${gtmId}"]').value ||
                   '0'
-                chrome.storage.local.set({ [gtmId]: { url: url.href, description, authuser } })
+                chrome.storage.local.set({ [containerId]: { url: url.href, description, authuser } })
               })
             )
 
             // Add event listeners for authuser changes
             document.querySelectorAll('.authuser-input').forEach(input =>
               input.addEventListener('change', e => {
-                const gtmId = e.target.dataset.gtmid
-                const description = document.querySelector(`.description-input[data-gtmid="${gtmId}"]`).input
-                const url = new URL(document.querySelector(`.url-input[data-gtmid="${gtmId}"]`).value)
+                const containerId = e.target.dataset.gtmid
+                const description = document.querySelector(
+                  `.description-input[data-containerid="${containerId}"]`
+                ).value
+                const url = new URL(document.querySelector(`.url-input[data-containerid="${containerId}"]`).value)
                 const authuser = String(Math.round(e.target.value))
-                chrome.storage.local.set({ [gtmId]: { url: url.href, description, authuser } })
+                chrome.storage.local.set({ [containerId]: { url: url.href, description, authuser } })
               })
             )
 
             // Add event listeners for open buttons
-            document.querySelectorAll('button[data-gtmid]').forEach(button =>
+            document.querySelectorAll('button[data-containerid]').forEach(button =>
               button.addEventListener('click', e => {
-                const gtmId = e.target.dataset.gtmid
-                const authuser = document.querySelector(`.authuser-input[data-gtmid="${gtmId}"]`).value
-                const url = new URL(document.querySelector(`.url-input[data-gtmid="${gtmId}"]`).value)
+                const containerId = e.target.dataset.gtmid
+                const authuser = document.querySelector(`.authuser-input[data-containerid="${containerId}"]`).value
+                const url = new URL(document.querySelector(`.url-input[data-containerid="${containerId}"]`).value)
                 url.searchParams.set('authuser', authuser)
                 chrome.tabs.create({ url: url.href })
               })
             )
           })
-        } else {
-          gtmList.innerHTML = '<li>Nenhum GTM encontrado.</li>'
         }
       }
     )
